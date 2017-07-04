685e46af61ef80029279dcd4a1f9ca488bc7b58f
lubin.xuan@gmail.com <xlb900907>
2017-07-05 01:36:44 +0800

    upload

---
+++ data-push-server/src/main/java/me/robin/crawler/push/DataPushServlet.java
@@ -0,0 +1,28 @@
+package me.robin.crawler.push;
+
+import org.apache.commons.io.IOUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * Created by LubinXuan on 2017/7/5.
+ */
+@WebServlet(name = "推送数据接收网关", value = "/push/data")
+public class DataPushServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String data = IOUtils.toString(req.getInputStream(), Charset.forName("utf-8"));
+        CrawlerDataDisruptor.ins.pushData(data);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("ok");
+    }
+}
--- data-push-server/src/main/java/me/robin/crawler/push/PushApplication.java
+++ data-push-server/src/main/java/me/robin/crawler/push/PushApplication.java
@@ -2,21 +2,14 @@ package me.robin.crawler.push;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * Created by LubinXuan on 2017/7/5.
  */
 @SpringBootApplication
-@Controller
+@ServletComponentScan("me.robin.crawler.push")
 public class PushApplication {
-
-    @PostMapping("push/data")
-    public void data(@RequestBody String data) {
-        CrawlerDataDisruptor.ins.pushData(data);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(PushApplication.class);

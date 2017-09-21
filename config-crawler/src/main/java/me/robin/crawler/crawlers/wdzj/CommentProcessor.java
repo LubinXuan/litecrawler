43233026202e31795eab0b4d56e2d11a806f5cd9
lubin.xuan@gmail.com <lubin.xuan@gmail.com>
2017-09-21 15:13:37 +0800

    网贷之家加入登录逻辑

--- config-crawler/src/main/java/me/robin/crawler/crawlers/wdzj/CommentProcessor.java
+++ config-crawler/src/main/java/me/robin/crawler/crawlers/wdzj/CommentProcessor.java
@@ -14,7 +14,6 @@ import org.slf4j.LoggerFactory;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Site;
-import us.codecraft.webmagic.selector.CssSelector;
 import us.codecraft.webmagic.selector.HtmlNode;
 import us.codecraft.webmagic.selector.Selectable;
 import us.codecraft.webmagic.utils.HttpConstant;
@@ -23,6 +22,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by Lubin.Xuan on 2017-07-05.
@@ -44,78 +44,110 @@ public class CommentProcessor extends RegexProcessor implements SitePrepare {
         return StringUtils.replaceEach(url, new String[]{"{pid}", "{page}"}, new String[]{platId, Integer.toString(page)});
     }
 
+    private Site site;
+
+    private final AtomicBoolean login = new AtomicBoolean(false);
+
     public CommentProcessor() {
         super("http://www.wdzj.com/front/dianpingInfo/(\\d+?)/20/(\\d+?)");
     }
 
     @Override
     public MatchOther processPage(Page page) {
-        HtmlNode htmlNode = (HtmlNode) page.getHtml().select(new CssSelector("ul.commentList div.bor"));
-        Integer commentLimit = TypeUtils.castToInt(page.getRequest().getExtra(Param.cursor_limit));
-        Integer commentCrawled = (Integer) page.getRequest().getExtra(Param.comment_crawled);
-        Integer updateCursor = (Integer) page.getRequest().getExtra(Param.cursor_limit_save);
-        if (null == commentCrawled) {
-            commentCrawled = 0;
-        }
-        String platId = StringUtils.substringBetween(page.getRequest().getUrl(), "dianpingInfo/", "/20/");
-        List<Map<String, Object>> commentList = new ArrayList<>();
-        for (Selectable comment : htmlNode.nodes()) {
-            int id = Integer.parseInt(StringUtils.replace(comment.$("span[id^=useful_]", "id").get(), "useful_", ""));
-            if (null != commentLimit && id <= commentLimit) {
-                break;
-            }
 
-            String plat = (String) page.getRequest().getExtra(Param.comment.platname);
-            if (null == updateCursor || updateCursor < id) {
-                updateCursor = id;
-                page.putField(Param.cursor_limit_save, id);
-                page.putField(Param.cursor_limit_update, true);
-                page.putField(Param.cursor_limit_key, plat);
-            }
-            String remark = comment.$("div.commentFont p.font", "allText").get();
-            String remarkTime = comment.$("span.date", "text").get();
-            String userName = comment.$("span.name", "allText").get();
+        Selectable loginMore = page.getHtml().$("a.loginMore");
 
-            Map<String, Object> commentMap = CralwData.commentData();
-            commentMap.put(Param.comment.platname, plat);
-            commentMap.put(Param.comment.remark, remark);
-            commentMap.put(Param.comment.remarktime, remarkTime + " 00:00:00");
-            commentMap.put(Param.comment.username, userName);
-            commentMap.put(Param.comment.headimg, comment.$("div.avatar img", "src").get());
-            commentMap.put(Param.comment.useful, comment.$("#useful_" + id, "text").get());
-            commentMap.put(Param.dataUid, platId + "-" + id);
-            String praise = StringUtils.trim(comment.$("span.tags", "text").get());
-            if (StringUtils.isBlank(praise)) {
-                praise = "一般";
-            }
-            commentMap.put(Param.comment.praise, PRAISE_ALIAS.getOrDefault(praise, praise));
-            commentList.add(commentMap);
-            commentCrawled++;
-        }
-        page.putField(DataPushPipeline.DATA_LIST, commentList);
-
-        String currentPage = page.getHtml().$("div.pageList a.on", "pagenumber").get();
-        String nextPage = page.getHtml().$("div.pageList a:containsOwn(下一页)", "pagenumber").get();
-        if (!StringUtils.equals(currentPage, nextPage)) {
-            Request request = new Request(page.getHtml().$("div.pageList", "url") + nextPage);
-            request.setMethod(HttpConstant.Method.POST);
-            request.setExtras(page.getRequest().getExtras());
-            request.putExtra(Param.comment_crawled, commentCrawled);
-            request.putExtra(Param.cursor_limit_save, updateCursor);
-            request.addHeader("referer", page.getRequest().getHeaders().get("referer"));
-            page.addTargetRequest(request);
+        if (!loginMore.nodes().isEmpty()) {
+            this.login();
+            page.addTargetRequest(page.getRequest());
         } else {
-            logger.info("评论爬取完成,共爬取评论数;{}   <-{}  {}", commentCrawled, commentLimit, page.getRequest().getHeaders().get("referer"));
+            HtmlNode htmlNode = (HtmlNode) page.getHtml().$("ul.commentList div.bor");
+            Integer commentLimit = TypeUtils.castToInt(page.getRequest().getExtra(Param.cursor_limit));
+            Integer commentCrawled = (Integer) page.getRequest().getExtra(Param.comment_crawled);
+            Integer updateCursor = (Integer) page.getRequest().getExtra(Param.cursor_limit_save);
+            if (null == commentCrawled) {
+                commentCrawled = 0;
+            }
+            String platId = StringUtils.substringBetween(page.getRequest().getUrl(), "dianpingInfo/", "/20/");
+            List<Map<String, Object>> commentList = new ArrayList<>();
+            for (Selectable comment : htmlNode.nodes()) {
+                int id = Integer.parseInt(StringUtils.replace(comment.$("span[id^=useful_]", "id").get(), "useful_", ""));
+                if (null != commentLimit && id <= commentLimit) {
+                    break;
+                }
+
+                String plat = (String) page.getRequest().getExtra(Param.comment.platname);
+                if (null == updateCursor || updateCursor < id) {
+                    updateCursor = id;
+                    page.putField(Param.cursor_limit_save, id);
+                    page.putField(Param.cursor_limit_update, true);
+                    page.putField(Param.cursor_limit_key, plat);
+                }
+                String remark = comment.$("div.commentFont p.font", "allText").get();
+                String remarkTime = comment.$("span.date", "text").get();
+                String userName = comment.$("span.name", "allText").get();
+
+                Map<String, Object> commentMap = CralwData.commentData();
+                commentMap.put(Param.comment.platname, plat);
+                commentMap.put(Param.comment.remark, remark);
+                commentMap.put(Param.comment.remarktime, remarkTime + " 00:00:00");
+                commentMap.put(Param.comment.username, userName);
+                commentMap.put(Param.comment.headimg, comment.$("div.avatar img", "src").get());
+                commentMap.put(Param.comment.useful, comment.$("#useful_" + id, "text").get());
+                commentMap.put(Param.dataUid, platId + "-" + id);
+                String praise = StringUtils.trim(comment.$("span.tags", "text").get());
+                if (StringUtils.isBlank(praise)) {
+                    praise = "一般";
+                }
+                commentMap.put(Param.comment.praise, PRAISE_ALIAS.getOrDefault(praise, praise));
+                commentList.add(commentMap);
+                commentCrawled++;
+            }
+            page.putField(DataPushPipeline.DATA_LIST, commentList);
+
+            String currentPage = page.getHtml().$("div.pageList a.on", "pagenumber").get();
+            String nextPage = page.getHtml().$("div.pageList a:containsOwn(下一页)", "pagenumber").get();
+            if (!StringUtils.equals(currentPage, nextPage)) {
+                Request request = new Request(page.getHtml().$("div.pageList", "url") + nextPage);
+                request.setMethod(HttpConstant.Method.POST);
+                request.setExtras(page.getRequest().getExtras());
+                request.putExtra(Param.comment_crawled, commentCrawled);
+                request.putExtra(Param.cursor_limit_save, updateCursor);
+                request.addHeader("referer", page.getRequest().getHeaders().get("referer"));
+                page.addTargetRequest(request);
+            } else {
+                logger.info("评论爬取完成,共爬取评论数;{}   <-{}  {}", commentCrawled, commentLimit, page.getRequest().getHeaders().get("referer"));
+            }
         }
         return MatchOther.NO;
     }
 
     @Override
     public void prepare(Site site) {
-        List<Cookie> cookieList = LoginUtil.login("18258837523", "1QaZ2WsX");
-        if (null != cookieList) {
-            cookieList.forEach(cookie -> site.addCookie(cookie.getName(), cookie.getValue()));
-            logger.info("登陆成功");
+        this.site = site;
+        this.login();
+    }
+
+    private void login() {
+        if (login.compareAndSet(false, true)) {
+            List<Cookie> cookieList = LoginUtil.login("18258837523", "1QaZ2WsX");
+            if (null != cookieList) {
+                cookieList.forEach(cookie -> site.addCookie(cookie.getName(), cookie.getValue()));
+                logger.info("登陆成功");
+            } else {
+                login.set(false);
+            }
+            synchronized (login) {
+                login.notifyAll();
+            }
+        } else {
+            synchronized (login) {
+                try {
+                    login.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
